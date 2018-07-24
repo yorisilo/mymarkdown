b3998d0355f3add9d25b840f61ac55d9866e29b2
yorisilo <yorisilo@gmail.com>
2018-07-24 23:50:32 +0900

    login logout

---
+++ src/components/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="editor">
+  <h1>Editor UI</h1>
+  <span>{{ user.displayName }}</span>
+  <button @click="logout">logout</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "editor",
+  props: ["user"],
+  data() {
+    return {}
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut()
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="home">
+  <h1>{{ msg }}</h1>
+  <button @click="googleLogin">Google アカウントでログイン</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "editor",
+  data() {
+    return {
+      msg: "Welcome to MyMarkdown"
+    }
+  },
+  methods: {
+    googleLogin: function() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    }
+  }
+}
+</script>
